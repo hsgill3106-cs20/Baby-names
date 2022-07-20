6e1a0bc4e9b5a3973e1a39c1bf3b4876ca12bc58
hsgill3106 <108752326+hsgill3106@users.noreply.github.com>
2022-07-20 12:40:41 -0600

    done

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let goBtn = document.getElementById("go-btn");
 let menuSelect = document.getElementById("menu-select");
 let container = document.getElementById("container");
 let nameCountSpan = document.getElementById("name-count");
-
+let number = document.getElementById("name-number");
 // Initialize Array of Character Objects from json file
 let babyData = [];
 fetch("baby-names-data.json")
@@ -38,27 +38,90 @@ function goBtnClicked() {
 
 // Display All Baby Names
 function displayAll() {
-  console.log("Display All");
-  // Confirm data load
-  console.log(babyData);
+  let allnameStr = "";
+  for (let i = 0; i < babyData.length; i++) {
+    allnameStr += nameHTML(babyData[i]);
+    nameCountSpan.innerHTML = allnameStr;
+    number.innerHTML = i;
+    console.log("Display All");
+    // Confirm data load
+    console.log(babyData);
+  }
 }
 
 // Display Names by Gender
 function searchGender() {
+  count = 0;
+  genderprompt = prompt("Please enter gender(Boy/Girl)");
+  let genderStr = "";
+  for (let i = 0; i < babyData.length; i++) {
+    if (babyData[i].gender === genderprompt) {
+      count++;
+      genderStr += nameHTML(babyData[i]);
+    }
+    number.innerHTML = count;
+  }
+  container.innerHTML = genderStr;
   console.log("Search By Gender");
 }
 
 // Display Names within a Range of Ranks
 function searchRank() {
+  let RankStr = "";
+  count = 0;
+  minprompt = prompt("Please enter minimum rank");
+  maxprompt = prompt("PLease enter maximum rank");
+  for (let i = 0; i < babyData.length; i++) {
+    ranks = babyData[i].rank;
+    if (ranks <= maxprompt && ranks >= minprompt) {
+      count++;
+      RankStr += nameHTML(babyData[i]);
+    }
+    number.innerHTML = count;
+  }
+  container.innerHTML = RankStr;
   console.log("Search By Rank");
 }
 
 // Display Names with Starting Letter
 function searchStartingLetter() {
+  count = 0;
+  let htmlStr = "";
+  typeprompt = prompt("Please enter starting letter");
+  for (let i = 0; i < babyData.length; i++) {
+    Names = babyData[i].name;
+    if (Names[0] === typeprompt) {
+      count++;
+      htmlStr += nameHTML(babyData[i]);
+    }
+    number.innerHTML = count;
+  }
+  container.innerHTML = htmlStr;
   console.log("Search by Starting Letter");
 }
 
 // Display Names with a Specific Length
 function searchLength() {
+  count = 0;
+  let searchlengthStr = "";
+  nameprompt = prompt("Please enter name length");
+  for (let i = 0; i < babyData.length; i++) {
+    namelength = babyData[i].name.length;
+    if (namelength == nameprompt) {
+      count++;
+      searchlengthStr += nameHTML(babyData[i]);
+    }
+    number.innerHTML = count;
+  }
+  container.innerHTML = searchlengthStr;
   console.log("Search by Name Length");
+}
+
+function nameHTML(x) {
+  return `
+<div>
+  <p>
+  <strong>${x.name}</strong> (Gender:${x.gender}, Rank:${x.rank})
+  </p>
+</div>`;
 }
